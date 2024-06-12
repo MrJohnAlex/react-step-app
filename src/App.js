e65ead3f684e0227f9e8b39bcb8f2841121710e0
MrJohnAlex <judi699095@gmail.com>
2024-06-13 00:00:47 +0700

    refactor: onclick function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
+import { useState } from "react";
+const message = ["Learn React", "Find a Job", "Learn Next JS"];
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+  function handleNextStep() {
+    if (step < 3) {
+      setStep((step) => step + 1);
+      setStep((step) => step + 1);
+    }
+  }
+  function handlePrevStep() {
+    if (step > 1) {
+      setStep((step) => step - 1);
+    }
+  }
   return (
     <div className="steps">
       <h1>Go step by step</h1>
       <div className="numbers">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
+        <p className={`${step >= 1 ? "active" : ""}`}>1</p>
+        <p className={`${step >= 2 ? "active" : ""}`}>2</p>
+        <p className={`${step >= 3 ? "active" : ""}`}>3</p>
       </div>
       <div className="message">
-        <p>Step {step}</p>
+        <p>Step: {message[step - 1]}</p>
       </div>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevStep}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNextStep}
+        >
           Next
         </button>
       </div>
